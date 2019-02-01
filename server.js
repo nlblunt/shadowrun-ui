24256ea07c822d50a26c643473d5788f223c30ab
nicolas.blunt <nicolas.blunt@gmail.com>
2019-01-31 20:28:55 -0500

    Change to server

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+//app.get('/*', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/dist/index.html'));
+//});
 
 // Run the app by serving the static files
 // in the dist directory

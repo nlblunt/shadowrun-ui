4ab9b995aefa555a1b33379857e8ffd089197369
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2019-02-11 23:22:01 -0800

    Started Runner html

---
+++ src/app/models/runner.ts
@@ -0,0 +1,23 @@
+export class Runner {
+  id: number;
+  name: string;
+  metatype:_id: number;
+  metatype: string;
+  agility: number;
+  charisma: number;
+  reaction: number;
+  body: number;
+  intuition: number;
+  wisdom: number;
+  strength: number;
+
+  essense: number;
+  magic: number;
+  edge: number;
+
+  karma: number;
+  careerKarma: number;
+
+  physicalDamage: number;
+  stunDamage: number;
+}
--- src/app/runner/runner.component.ts
+++ src/app/runner/runner.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+//MODELS
+import ( Runner } from '../models/runner';
+
 @Component({
   selector: 'app-runner',
   templateUrl: './runner.component.html',
   styleUrls: ['./runner.component.css']
 })
 export class RunnerComponent implements OnInit {
+  creatingNewRunner: boolean = false;
+  newRunner: Runner;
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+  //Sets if we are creating a new runner for the user
+  SetNewRunner(state: boolean)
+{
+	this.creatingNewRunner = state;
+	
+	//If we are creating a new runner, initialize newRunner
+	if(state == true)
+	{
+		this.newRunner = new Runner;
+	}
+}
+
 
 }
